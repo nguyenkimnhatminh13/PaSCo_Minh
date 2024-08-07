bb9f57a7cdd9eeb54d5d88878716248d1d8e8e89
vuong-viet-hung <vuongviethung156@gmail.com>
2024-08-07 15:55:40 +0900

    Only iterate over scans whose generated labels are available

--- scripts/save_labels.py
+++ scripts/save_labels.py
@@ -14,37 +14,44 @@ VOXEL_SIZE = 0.2
 
 
 @click.command()
-@click.option("--root_dir", default="gpfsdswork/dataset/SemanticKITTI/dataset/sequences")
-@click.option("--config_path", default="semantic-kitti.yaml")
+@click.option("--root", default="gpfsdswork/dataset/SemanticKITTI/dataset/sequences")
 @click.option("--output_dir", default="output/sequences")
-def main(root_dir: str, config_path: str, output_dir: str) -> None:
+@click.option(
+    "--gen_label_root", 
+    default="gpfsscratch/rech/kvd/uyl37fq/pasco_preprocess/gt_modified/instance_labels_v2",
+)
+def main(root: str, output_dir: str, gen_label_root: str) -> None:
 
-    # Load config
-    with open(config_path) as config_file:
-        config = yaml.safe_load(config_file)
-    sequences = config["split"]["train"] + config["split"]["valid"]
+    sequence_dir_names = os.listdir(gen_label_root)
+    sequence_prog_bar = tqdm(sequence_dir_names)
+    for sequence_idx, sequence_dir_name in enumerate(sequence_prog_bar, start=1):
+        sequence_prog_bar.set_description(
+            f"Sequence {sequence_idx}/{len(sequence_dir_names)}"
+        )
 
-    sequence_prog_bar = tqdm(sequences)
-    for sequence_idx, sequence in enumerate(sequence_prog_bar, start=1):
-        sequence_prog_bar.set_description(f"Sequence {sequence_idx}/{len(sequences)}")
-
-        scan_dir = os.path.join(root_dir, f"{sequence:02}", "velodyne")
-        scan_names = os.listdir(scan_dir)
-        scan_prog_bar = tqdm(scan_names, leave=False)
-        for scan_idx, scan_name in enumerate(scan_prog_bar, start=1):
-            scan_prog_bar.set_description(f"Scan {scan_idx}/{len(scan_names)}")
+        gen_label_dir = os.path.join(gen_label_root, sequence_dir_name)
+        gen_label_names = os.listdir(gen_label_dir)
+        scan_prog_bar = tqdm(gen_label_names, leave=False)
+        for scan_idx, gen_label_name in enumerate(scan_prog_bar, start=1):
+            scan_prog_bar.set_description(f"Scan {scan_idx}/{len(gen_label_names)}")
 
             # Load scan
-            scan_path = os.path.join(scan_dir, scan_name)
+            scan_dir = os.path.join(root, sequence_dir_name, "velodyne")
+            scan_path = (
+                os.path.join(scan_dir, gen_label_name)
+                .replace("_1_1", "")
+                .replace("pkl", "bin")
+            )
             scan = np.fromfile(scan_path, dtype=np.float32).reshape(-1, 4)
             coords = scan[:, :3]
             feats = scan[:, 3:]
 
             # Load labels
+            label_dir = os.path.join(root, sequence_dir_name, "labels")
             label_path = (
-                scan_path
-                .replace("velodyne", "labels")
-                .replace("bin", "label")
+                os.path.join(label_dir, gen_label_name)
+                .replace("_1_1", "")
+                .replace("pkl", "label")
             )
             labels = np.fromfile(label_path, dtype=np.uint32)
             semantic_labels = labels & 0xFFFF
@@ -71,7 +78,7 @@ def main(root_dir: str, config_path: str, output_dir: str) -> None:
             # Save discrete coords
             discrete_coords_path = (
                 scan_path
-                .replace(root_dir, output_dir)
+                .replace(root, output_dir)
                 .replace("velodyne", "discrete_coords")
             )
             os.makedirs(os.path.dirname(discrete_coords_path), exist_ok=True)
@@ -79,7 +86,7 @@ def main(root_dir: str, config_path: str, output_dir: str) -> None:
 
             # Save scan
             output_scan = np.hstack([unique_coords, unique_feats])
-            output_scan_path = scan_path.replace(root_dir, output_dir)
+            output_scan_path = scan_path.replace(root, output_dir)
             os.makedirs(os.path.dirname(output_scan_path), exist_ok=True)
             output_scan.tofile(output_scan_path)
 
@@ -88,7 +95,7 @@ def main(root_dir: str, config_path: str, output_dir: str) -> None:
                 (unique_instance_labels << 16)
                 | (unique_semantic_labels & 0xFFFF)
             )
-            output_label_path = label_path.replace(root_dir, output_dir)
+            output_label_path = label_path.replace(root, output_dir)
             os.makedirs(os.path.dirname(output_label_path), exist_ok=True)
             output_labels.tofile(output_label_path)
 
